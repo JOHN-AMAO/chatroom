387e1c313f7405a882c2514b6c2c2b29ca90d621
JOHN-AMAO <amaojohn767@gmail.com>
2024-02-25 19:26:52 +0100

    bug fixes

--- base/models.py
+++ base/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Room(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextFeld(null=True)
+    description = models.TextField(null=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
